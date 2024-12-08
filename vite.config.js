3e5ff8bfb9dbe39dcf762846660b51c75de03b42
yuapX <shesterovmm@gmail.com>
2024-12-08 15:22:40 +0200

    update nginx.conf

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,23 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
-    basicSsl(),
+    // basicSsl(),
     svgr(),
   ],
   publicDir: './public',
   server: {
     host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
   },
   assetsInclude: ['**/*.tgs'],
   resolve: {
