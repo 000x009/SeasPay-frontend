02f18bf799722877618d16cf6e5e8dc59d930133
yuapX <shesterovmm@gmail.com>
2024-12-09 23:10:28 +0200

    back to master

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
-    // basicSsl(),
     svgr(),
   ],
   publicDir: './public',
