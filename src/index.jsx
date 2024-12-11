57806769e32320a40a276e8304c174b76f9d5b63
yuapX <shesterovmm@gmail.com>
2024-12-11 20:03:04 +0200

    some styles again

--- src/index.jsx
+++ src/index.jsx
@@ -10,13 +10,12 @@ const telegram = window.Telegram.WebApp;
 
 ReactDOM.createRoot(root).render(<Root/>);
 
-if (telegram.platform.startsWith('web')) {
+if (['webk', 'weba', 'tdesktop'].includes(telegram.platform)) {
   if (telegram.colorScheme === 'dark') {
     root.style.setProperty('--tg-theme-bg-color', '#0f0f0f');
     root.style.setProperty('--tg-theme-secondary-bg-color', '#212121');
   }
   if (telegram.colorScheme === 'light') {
-    root.style.setProperty('--tg-theme-bg-color', '#EFEEF4');
     root.style.setProperty('--tg-theme-secondary-bg-color', '#FFFFFF');
   }
 }
--- src/scripts/helpers/setTheme.js
+++ src/scripts/helpers/setTheme.js
@@ -3,18 +3,12 @@ import { darkCustomTheme } from "@/constants/theme";
 
 export function setTheme() {
   const { WebApp } = useTelegram();
-  console.log("platform", WebApp.platform);
-  console.log("colorScheme", WebApp.colorScheme);
 
-  if (WebApp.platform.startsWith('web')) {
-    console.log("set colorScheme");
+  if (['webk', 'weba', 'tdesktop'].includes(WebApp.platform)) {
     if (WebApp.colorScheme === 'dark') {
       WebApp.setBackgroundColor(darkCustomTheme.bgColor);
       WebApp.setHeaderColor(darkCustomTheme.bgColor);
       WebApp.setBottomBarColor(darkCustomTheme.bgColor);
-    }
-    if (WebApp.colorScheme === 'light') {
-      WebApp.setBottomBarColor(WebApp.themeParams.secondary_bg_color);
     }
   }
 }
